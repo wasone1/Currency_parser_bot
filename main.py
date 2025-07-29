788f15aa5815c2993001f390b1d4b9ed062bac68
wasone1 <wasone1987@gmail.com>
2025-07-29 11:58:35 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ from aiogram.types import BotCommand
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
 def run_stub_server():
     class Handler(BaseHTTPRequestHandler):
         def do_GET(self):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"OK")
+
+        def do_HEAD(self):
+            self.send_response(200)
+            self.end_headers()
+
+        def log_message(self, format, *args):
+            return  # Вимикаємо логування у консоль
+
     server = HTTPServer(("0.0.0.0", 8080), Handler)
     server.serve_forever()
 
